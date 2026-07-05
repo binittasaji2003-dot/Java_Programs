9b1da5977ab8863e47402163f05926766757beb8
binittasaji2003-dot <binittasaji2003@gmail.com>
2026-07-05 12:50:15 +0530

    Using Nested if, predefined variables, Loan Approval

---
+++ LoanApproval.java
@@ -0,0 +1,17 @@
+// Nested if
+// Loan Approval
+// predefined variables
+
+public class LoanApproval {
+    public static void main(String[] args){
+        int salary = 40000;
+        int creditScore = 750;
+
+        if (salary >= 30000) {
+            if (creditScore >= 700) {
+                System.out.println("Loan Approval");
+            }
+        }
+
+    }
+}
